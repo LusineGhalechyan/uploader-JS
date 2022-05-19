d94d698df6d9ac6adf5eb479fcd851814a64f936
Lusine Ghalechyan <ghalechyan@yahoo.com>
2022-05-20 02:16:52 +0400

    implementing parallel, chunk wise async logic

--- script/index.js
+++ script/index.js
@@ -23,6 +23,14 @@ class Uploader {
     return Uploader.container.querySelector(el);
   }
 
+  static createEl(el) {
+    return Uploader.container.createElement(el);
+  }
+
+  static setAttrToEl(el, attr, val) {
+    return Uploader.createEl(el).setAttribute(attr, val);
+  }
+
   upload() {
     // console.log(`cont`, Uploader.container);
     Uploader.el(".uploader__button--upload").addEventListener("change", () => {
@@ -37,58 +45,56 @@ class Uploader {
 
   transfer() {
     var chunk = [];
-    console.log(`CHUNK_INIT`, chunk);
+    var promises = [];
+    // console.log(`CHUNK_INIT`, chunk);
     Uploader.el(".uploader__button--transfer").addEventListener("click", () => {
-      // if (this.formDataData.length) {
-      //   this.formDataData = this.formDataData.filter((el) => el !== "{}");
-      // }
       this.formDataData = Object.values(JSON.parse(this.formDataData));
 
       for (let i = 0; i < this.formDataData.length; i += this.#chunkSize) {
         chunk = this.formDataData.slice(i, i + this.#chunkSize);
         console.log(`CHUNK_FIN`, chunk);
-        var request = new XMLHttpRequest();
+        promises.push(
+          new Promise((resolve, reject) => {
+            var request = new XMLHttpRequest();
 
-        request.open("POST", this.#api);
-        request.send(chunk);
+            request.open("POST", this.#api);
+            request.send(chunk);
 
-        request.onload = function () {
-          console.log(`Loaded: ${request.status} ${request.response}`);
-        };
+            request.onload = function () {
+              if (request.status >= 200 && request.status < 300) {
+                resolve(request.response);
+              } else {
+                reject(request.statusText);
+              }
+              // console.log(`Loaded: ${request.status} ${request.response}`);
+            };
 
-        request.onerror = function () {
-          alert(`‼ Failed to post data`);
-        };
-
-        request.onprogress = function (evt) {
-          evt.preventDefault();
-          var computedLength = evt.lengthComputable;
-          var totalEvts = evt.total;
-          var loadedEvts = evt.loaded;
-          console.log(`loadedEvts`, loadedEvts);
-          console.log(`totalEvts`, totalEvts);
-          if (computedLength) {
-            var loadedPercent = (loadedEvts / totalEvts) * 100;
-            Uploader.el(".uploader__progressBar").value = loadedPercent;
-            Uploader.el(
-              ".uploader__loadedTotal"
-            ).innerHTML = `${loadedPercent}%`;
-          }
-        };
+            request.onerror = function () {
+              reject(request.statusText);
+            };
+            
+            var loadedPercent =
+              (1 / Math.ceil(this.formDataData.length / this.#chunkSize)) * 100;
+            if (loadedPercent) {
+              var newValue =
+                Uploader.el(".uploader__progressBar").value + loadedPercent;
+              Uploader.el(".uploader__progressBar").value = newValue;
+              Uploader.el(".uploader__loadedTotal").innerHTML = `${Math.round(
+                newValue
+              )}%`;
+            }
+          })
+        );
       }
+      Promise.all(promises)
+        .then((responses) => {
+          console.log(responses);
+        })
+        .catch((err) => console.log("ERROR", err));
     });
-    // console.log(`f_data`, formData.get("file"));
   }
 }
 
 var uploader = new Uploader();
 uploader.upload();
 uploader.transfer();
-
-// function onDragStart(evt) {}
-// function onDragOver(evt) {}
-
-// uploader.ondragover = function (evt) {
-//   evt.preventDefault();
-//   evt.target.style.border = "3px dotted blue";
-// };
